a80b28aec271b76fa3565643db5d510f81e14e52
Jerry Gao <gao@blackwright.com>
2017-02-12 22:30:02 -0500

    add logger

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var _ = require("lodash");
 var chalk = require("chalk");
+var logger = require("./lib/logger");
+
 
 var names = ["Jerry", "Maggie", "Emily"];
-
 _.each(names, function(name) {
   console.log(chalk.cyan("Hello %s!"), name);
 });
+
+console.log("Testing logger:");
+logger.log("Here's some info", "INFO");
+logger.log("Here's a warning", "WARNING");
+logger.log("Total failure!", "ERROR");
