387f79c8a4c7b6d8491cd8ddeddca4268d932824
Egle Libby <egle.ciparyte@gmail.com>
2017-02-25 11:05:35 -0500

    build the logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var chalk = require ('chalk');
 var _ = require ('lodash');
+var logger = require('./lib/logger');
 
 console.log('Hello world!');
 console.log(chalk.blue('Hello world!'));
@@ -7,3 +8,5 @@ console.log(chalk.blue('Hello world!'));
 _.each([1,2,3], function(value){
   console.log(value);
 });
+
+logger.log("Hello world!", "warning");
