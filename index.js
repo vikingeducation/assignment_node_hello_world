2bee4fe265928c3fd3d5c8ae9d2c2491363946c8
Ann Allan <ann.cristina.allan@gmail.com>
2017-03-04 17:41:20 -0500

    Add module from logger file to index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var chalk = require('chalk');
-var _ = require('lodash')
-var red = chalk.red;
-var players = [62, 87, 43];
-console.log(red('Hello World!'));
-_.each(players, function (player) {
-  console.log(player);
-});
+// var chalk = require('chalk');
+// var _ = require('lodash')
+// var red = chalk.red;
+// var players = [62, 87, 43];
+// console.log(red('Hello World!'));
+// _.each(players, function (player) {
+//   console.log(player);
+// });
+var logger = require('./lib/logger');
+logger.log('I am info', 'INFO');
+logger.log('I am a warning', 'WARNING');
+logger.log('I am an error', 'ERROR');
