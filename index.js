9af6f5f26c42301f796a5562378eec9a1900576d
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-13 13:39:48 -0700

    Add logger.js to log with log-specific colors

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger');
 
-var error = chalk.bold.red;
-console.log(error('Error!'));
-console.log(chalk.blue('Hellow blue world!'));
-console.log('Hello world!');
+// var error = chalk.bold.red;
+// console.log(error('Error!'));
+// console.log(chalk.blue('Hellow blue world!'));
+// console.log('Hello world!');
+//
+// var numbers = [1, 5, 10, 50];
+// var log = function (element) {
+//   console.log(element);
+// };
+//
+// _(numbers).forEach(log);
+//
+// console.log(_.includes([1, 2, 3], 5));
 
-var numbers = [1, 5, 10, 50];
+// logger.log("This is info", "info");
+// logger.log("This is a warning", "warning");
+// logger.log("This is an error", "error");
 
-console.log(_.includes([1, 2, 3], 5));
+logger.log("cat", "info");
+logger.log("dog", "warning");
+logger.log("soup", "error");
