086029f8f34754192135b74b209707f328cad8fe
William Whitworth <william10charles@gmail.com>
2017-03-14 16:02:07 -0600

    functioning logger, logger requires chalk

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var chalk = require("chalk");
 var _ = require("lodash");
+var logger = require("./lib/logger");
 
 var testArr = [1, 2, 3, 4, 5, 6];
 console.log(chalk.blue("hello world"));
 _.each(testArr, function(item) {
   console.log(item);
 });
+
+logger.log("what is this thing", "ERROR");
