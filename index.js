b80e5ad9e901a9d10708d23dc55ca4ca7944fb47
Chris Banning <chrisbanning510@gmail.com>
2017-03-19 02:13:40 -0700

    node.js hello world

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');
 var _ = require('lodash');
+var logs = require('./data/logs.json');
 
 var logger = require('./lib/logger.js');
 
-logger.info('blue');
-logger.warning('yellow');
-logger.error('red');
+// logger.info('blue');
+// logger.warning('yellow');
+// logger.error('red');
+
+_.each(logs, function(log) {
+  if (log.level === "info") {
+    logger.info(log.message);
+  }
+  if (log.level === "warning") {
+    logger.warning(log.message);
+  }
+  if (log.level === "error") {
+    logger.error(log.message);
+  }
+});
 
 
 
