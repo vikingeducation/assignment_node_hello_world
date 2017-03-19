0496dc7672467b684c3452fa77d51b973c932987
Wesley <wesleyrasada@gmail.com>
2017-03-19 10:43:14 -0700

    Add logger for JSON data

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var chalk   = require('chalk'),
-    _       = require('lodash');
+var _       = require('lodash'),
+    logger  = require('./lib/logger'),
+    data    = require('./data/logs');
+
+_.each(data, function(data){
+  var level = data.level;
+  var message = data.message;
+  logger.log(message, level);
+});
+
+logger.info("Information test");
+logger.warn("Warning test");
+logger.error("Error test");
