e8f6ae3b7c185ec295690951e068b898e0f8b96b
markmarkyesyes <markhahn545@gmail.com>
2017-03-19 21:00:32 -0500

    Completed logger with color levels

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var chalk = require('chalk');
 var _ = require('lodash');
-// var array = require('lodash/array');
+var logger = require('./lib/logger.js');
 
 console.log(chalk.blue("hello world"));
 
 _.each([1,2,3], function(num) {
   console.log(chalk.blue(num));
-})
+});
+
+logger("hello there", "info");
+logger("oh boy", "warning");
+logger("shit shit shit", "error")
