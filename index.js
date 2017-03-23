57c473e8c100e202e65d2ba96b19554af70a49c2
DHail <dhail@ymail.com>
2017-03-22 23:25:54 -0700

    JSON added

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var _ = require('lodash');
 chalk = require('chalk');
 var Logger = require('./lib/logger');
+var logs = require('./data/logs');
+
 
 var logger = new Logger;
 logger.log("Message logged at INFO level", "INFO");
 logger.log("Message logged at WARNING level", "WARNING");
 logger.log("Message logged at ERROR level", "ERROR");
+
+_.each(logs, function(record) {
+	logger.log(record["message"], record["level"]);
+});
