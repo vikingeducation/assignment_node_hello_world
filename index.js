3f0e7c5d8775620c4765b3455c3803c5d1854ece
Bill Hefty <billhefty@gmail.com>
2017-03-26 10:11:35 -0500

    implement data.json

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const _ = require('lodash');
 const logger = require('./lib/logger');
+const data = require('./data/logs');
 
 let messageLevel = ['info', 'warning', 'error'];
 
 _.each(messageLevel, (level) => {
     logger.log(`Message logged at ${level.toUpperCase()} level`, level);
+});
+
+_.each(data, (data) => {
+    logger.log(data.message, data.level);
 });
