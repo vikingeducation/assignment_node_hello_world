cc105716a75e7f74a35e79bf17986bc8ad39abc4
rabGIT <rabGIT@users.noreply.github.com>
2017-03-30 16:11:15 -0700

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const _ = require('lodash');
 const chalk = require('chalk');
 const logger = require('./lib/logger');
+const logs = require('./data/logs');
+
+
+// test array for requires
 var testArray = [1,2,3,"..."];
 
 console.log(chalk.red.bold('Hello World!'));
@@ -9,8 +13,15 @@ _.each(testArray, function(arrayItem){
   console.log(arrayItem + ' ');
 });
 
+// testing logger module
 logger.log("test message info", "INFO");
 
 logger.log("test message warn", "WARNING");
 
 logger.log("test message error", "ERROR");
+
+
+// testing against JSON data
+_.each(logs, function(logline){
+  logger.log(logline.message, logline.level.toUpperCase())
+});
