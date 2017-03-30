dfed9d026563e9d7986f11918d17bef2372bd6a8
rabGIT <rabGIT@users.noreply.github.com>
2017-03-30 16:06:46 -0700

    implement logger with colorized output

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const chalk = require('chalk');
+const logger = require('./lib/logger');
 var testArray = [1,2,3,"..."];
 
 console.log(chalk.red.bold('Hello World!'));
@@ -7,3 +8,9 @@ console.log(chalk.red.bold('Hello World!'));
 _.each(testArray, function(arrayItem){
   console.log(arrayItem + ' ');
 });
+
+logger.log("test message info", "INFO");
+
+logger.log("test message warn", "WARNING");
+
+logger.log("test message error", "ERROR");
