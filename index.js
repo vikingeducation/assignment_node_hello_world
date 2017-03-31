483cf8af5c98fb41e0bf14c168ec1d9c018521fd
Chris Banning <chrisbanning510@gmail.com>
2017-03-31 17:25:50 -0400

    completed Node.js Hello World! assignment

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var chalk = require('chalk');
+var _ = require('lodash');
 var logger = require('./lib/logger.js');
+var logs = require('./data/logs');
 
-logger.log('milk', 'INFO');
-logger.log('eggs', 'WARNING');
-logger.log('bread', 'ERROR');
+//console.log(logs);
+
+// logger.log('milk', 'INFO');
+// logger.log('eggs', 'WARNING');
+// logger.log('bread', 'ERROR');
+
+_.each(logs, function(log) {
+  logger.log(log.message, log.level);
+});
 
 
 
