0733836a495509dc8a7e03e4692de1d15d3fb621
Edwin Padron <4nkaci6@gmail.com>
2017-04-13 21:54:48 -0500

    Add logger.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 var chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger');
 console.log(chalk.red("Hello world!"));
 console.log(chalk.yellow.bold("Welcome to my node.js app"));
 
@@ -7,3 +9,7 @@ var countries = ["Brazil", "Argentina", "Spain", "England"];
 _.each(countries, function(country) {
   console.log(country);
 });
+
+logger.log("Message logged at INFO level", "INFO");
+logger.log("Message logged at WARNING level", "WARNING");
+logger.log("Message logged at ERROR level", "ERROR");
