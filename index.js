ff571f4f0ab2390d8098dff91d60968b5fed1e9d
Christian Florez <florez.christian@gmail.com>
2017-04-16 15:18:24 -0400

    add basic logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var chalk = require("chalk");
 var _ = require("lodash");
+var logger = require('./lib/logger');
 
 
 var greeting = chalk.bold.blue('Hello ') + chalk.bgCyan('World' + chalk.red('!'));
@@ -10,3 +11,11 @@ var myArray = ["Greetings", "to", "you", "dear", "earth!"];
 _.each(myArray, function(element) {
     console.log(element);
 });
+
+logger.log("Hola, mundo!", "INFO");
+logger.log("Bonjour, monde!", "WARNING");
+logger.log("Hallo, welt!", "ERROR");
+
+logger.info("Hola, mundo!");
+logger.warning("Bonjour, monde!");
+logger.error("Hallo, welt!", "ERROR");
