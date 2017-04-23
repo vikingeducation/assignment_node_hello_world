d13ee1f468a5cac6f564bb86716a6c4ee4f96dea
Lorraine Chan <lchan912@gmail.com>
2017-04-23 16:22:14 -0700

    Refactor

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const chalk = require('chalk');
 const _ = require('lodash');
 
-const messages = require('./data/logs');
+const logger = require('./lib/logger.js');
+const logs = require('./data/logs.json');
 
-const logger = {
-  level: {
-    'info': function(msg) { return chalk.blue(msg) },
-    'warning': function(msg) { return chalk.yellow(msg) },
-    'error': function(msg) { return chalk.red(msg) }
-  },
-
-  print: function(msg, level) { console.log(logger.level[level](msg)); }
-};
-
-_.each(messages, (msg) => {
-  logger.print(msg.message, msg.level);
+_.each(logs, (log) => {
+  logger.log(log.message, log.level);
 });
