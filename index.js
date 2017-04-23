44bf67fde054c5f6067adf77cab42505bdb5df68
Lorraine Chan <lchan912@gmail.com>
2017-04-23 15:55:30 -0700

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const chalk = require('chalk');
 const _ = require('lodash');
+
+const messages = require('./data/logs');
+
+const logger = {
+  level: {
+    'info': function(msg) { return chalk.blue(msg) },
+    'warning': function(msg) { return chalk.yellow(msg) },
+    'error': function(msg) { return chalk.red(msg) }
+  },
+
+  print: function(msg, level) { console.log(logger.level[level](msg)); }
+};
+
+_.each(messages, (msg) => {
+  logger.print(msg.message, msg.level);
+});
