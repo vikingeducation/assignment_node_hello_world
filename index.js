33de11f0c3a154de827496abee1eff6416ea8bb5
tt <ttwang.303@gmail.com>
2017-04-26 04:04:34 -0400

    logger module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var chalk = require("chalk");
 var _ = require("lodash");
+var logger = require("./lib/logger")
 
 // chalk test
 console.log(chalk.bold.magenta("hello world"))
@@ -10,3 +11,8 @@ var nums = [1,2,3];
 _.each(nums, function(num) {
   console.log(chalk.underline.white("Number " + num));
 });
+
+// logger module
+logger.log("hello world", "INFO")
+logger.log("hello world", "WARNING")
+logger.log("hello world", "ERROR")
