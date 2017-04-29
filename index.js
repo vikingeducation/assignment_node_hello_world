1dc87bc76d7f276eb16103b0d7683195520a3876
Tyler Ketron <tketron@gmail.com>
2017-04-29 15:04:28 -0700

    verified functions packages

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+var chalk = require('chalk');
+var _ = require('lodash');
+
+var testArray = ["Tyler", "Mattie", "Cora", "Katie", "Carl"];
+console.log(chalk.blue('Hello world!'));
+_.each(testArray, function(name) {
+  console.log(chalk.red(name));
+});
