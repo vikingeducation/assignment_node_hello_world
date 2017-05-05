8c719de46cf8a32e0f6f0692b9f550c805e1fc26
Andrew Knox <aknox0329@gmail.com>
2017-05-05 14:10:54 -0700

    logger functioning at all levels

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var chalk = require('chalk');
 var _ = require('lodash');
 var logger = require('./lib/logger');
 var salutations = require('./lib/salutations');
 
-_.each(salutations, function(salutation) {
-    logger.log(salutation);
-});
+logger.log("Message logged at INFO level", "info");
+logger.log("Message logged at WARNING level", "warning");
+logger.log("Message logged at ERROR level", "error");
