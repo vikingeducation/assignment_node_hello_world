d91ce6b47a9f10d7dfff6c015addc2663f7f4c69
Andrew Chong <opheron@gmail.com>
2017-05-30 15:57:42 -0400

    Create logging functions

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const chalk = require('chalk');
-const _ = require('lodash');
+// const chalk = require('chalk');
+// const _ = require('lodash');
+const logger = require('./lib/logger');
 
-console.log(chalk.blue('Hello world!'));
+// console.log(chalk.blue('Hello world!'));
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-_.each(arr, (value) => console.log(value));
+// _.each(arr, value => console.log(value));
+
+logger.info('Info level');
+logger.warning('Warning');
+logger.error('Error');
