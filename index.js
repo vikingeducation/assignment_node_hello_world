8175c450303b4a0fab5b041c637623f9b41bb93c
Luke E Summerscales <ke2uke@virginmedia.com>
2017-06-01 10:55:18 +0100

    finished

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var logger = require("./lib/logger");
-var chalk = require("./node_modules/chalk");
 var lodash = require("./node_modules/lodash");
+var logs = require("./data/logs");
 
-var myA = ["x", "y", "z"];
-lodash._.forEach(myA, function(value) {
-  console.log(value);
+lodash._.forEach(logs, function(log){
+  logger.log(log.message, log.level);
 });
-
-logger.log("Hello World!");
-console.log(chalk.blue("peekaboo"));
