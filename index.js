d6856e9ea1cca5b11c66e772a6667312aef60fe8
Luke E Summerscales <ke2uke@virginmedia.com>
2017-06-01 10:29:09 +0100

    chalk and lodash installed and working

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var logger = require("./lib/logger");
+var chalk = require("./node_modules/chalk");
+var lodash = require("./node_modules/lodash");
+
+var myA = ["x", "y", "z"];
+lodash._.forEach(myA, function(value) {
+  console.log(value);
+});
 
 logger.log("Hello World!");
+console.log(chalk.blue("peekaboo"));
