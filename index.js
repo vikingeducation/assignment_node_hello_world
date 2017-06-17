8c127e69875a3ce00890c6259e95e232479bda4e
Alex-Willenbrink <awillenbrink@gmail.com>
2017-06-17 17:24:04 -0500

    finished assignment

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ let chalk = require("chalk");
 let _ = require("lodash");
 
 // Test the chalk package first
-console.log(chalk.yellow("Hello world!"));
+//console.log(chalk.yellow("Hello world!"));
 
 // Tes the lodash package second
+/*
 let array = [1,2,3,4,5];
 _.each(array, function(value){
   console.log("value: " + value);
 });
-
+*/
 
 // TIME FOR LOGGING DUTIES
 let logger = require("./lib/logger");
-logger("Message logged at INFO level", "INFO");
-logger("Message logged at WARNING level", "WARNING");
-logger("Message logged ERROR level", "ERROR");
+let data = require("./data/logs");
+
+_.each(data, function(value){
+  logger(value["message"], value["level"]);
+});
