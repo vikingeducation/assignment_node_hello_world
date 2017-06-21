a350afc0df604149d3c39314d7af6582cc961137
Kara Thrash <dvdonato@gmail.com>
2017-06-21 14:32:39 -0400

    chalk and lodash added

---
+++ index.js
@@ -0,0 +1,9 @@
+var lodash = require('lodash');
+var array = require('lodash/array');
+var chalk = require('chalk');
+
+console.log(chalk.blue('Hello world!'));
+lodash.each();
+for(var i = 0; i < 5; i++) {
+    console.log(i);
+}
