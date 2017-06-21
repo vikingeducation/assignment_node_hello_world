0163cd64e7388c0572e02d092eee4650c40d602e
Kara Thrash <dvdonato@gmail.com>
2017-06-21 15:16:33 -0400

    log messages

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var lodash = require('lodash');
 var array = require('lodash/array');
 var chalk = require('chalk');
-
+var logger = require('./lib/logger');
+var salutations = "hey you";
 console.log(chalk.blue('Hello world!'));
 lodash.each();
 for(var i = 0; i < 5; i++) {
     console.log(i);
 }
+logger.log(salutations,0);
+logger.log(salutations,2);
+logger.log(salutations,1);
