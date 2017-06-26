e125aadb8a2e92d2382af5b77124409bc972dfdf
Benjamin Soung <soungbenjamin@gmail.com>
2017-06-26 14:14:56 -0400

    finished loggger module

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var _ = require('lodash');
 var chalk = require('chalk');
+var logger = require('./lib/logger');
 
 console.log(chalk.blue('Hello world!'));
 
 _.each([1,2,3,4,5], function(n) {
 	console.log(n);
 })
+
+logger.log('hello info', 'info');
+logger.info('information');
+logger.warning('danger');
+logger.error('uh oh');
