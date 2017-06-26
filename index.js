8cfcf607c24b675ee33e8245a7394e1acdadff81
Benjamin Soung <soungbenjamin@gmail.com>
2017-06-26 14:19:43 -0400

    finished json logging

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var _ = require('lodash');
 var chalk = require('chalk');
 var logger = require('./lib/logger');
+var json = require('./data/logs.json');
 
+_.each(json, function(el) {
+	if (el.level === 'error') {
+		logger.error(el.message);
+	} else if (el.level === 'warning') {
+		logger.warning(el.message);
+	} else if (el.level === 'info') {
+		logger.info(el.message);
+	} else {
+		console.log(el.message);
+	}
+})
+
+
+// basic tests
 console.log(chalk.blue('Hello world!'));
 
 _.each([1,2,3,4,5], function(n) {
