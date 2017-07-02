4056105a4f13e3c026e12db3d3c3fc7ed27c26c4
Will Timpson <will.timpson@gmail.com>
2017-07-02 11:57:02 -0400

    Log from logs.json

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 
 let _ = require('lodash');
 let logger = require('./lib/logger.js');
+let logs = require('./data/logs.json');
 
+_.each(logs, function(log) {
+  logger[log.level](log.message);
+} )
