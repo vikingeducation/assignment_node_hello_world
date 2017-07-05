690a59e0afc9325f2a58537e555be1919e5bd529
Eric Glover <etgt28@mail.missouri.edu>
2017-07-04 19:33:29 -0500

    logger exported and functioning

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ _.each(t, function(t) {
 })
 
 log(chalk.blue("The smurfs are coming") + "..." + chalk.red("BEWARE!"));
+
+var logger = require('./lib/logger');
+
+log("hello");
+logger.info("hello matez");
+logger.log("Hello World!", "INFO");
+logger.log("You may have forgotten an s in this line\n'module.export = logger;'", "WARNING");
+logger.log("Program received signal SIGBUS, Bus error", "ERROR");
