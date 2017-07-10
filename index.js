818846a5fa6e675e7710c77608d436da1a5d6689
Colin Knebl <colin.knebl@outlook.com>
2017-07-09 21:14:09 -0400

    project complete

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var _ = require('lodash');
-
+var logs = require('./data/logs');
 var logger = require('./lib/logger');
 
 
 
-logger.log('this is how we do!', 'warning');
-logger.info("how do we do?");
-logger.warning('this is how we do!');
-logger.error('wait, I did not get all that.');
+_.each(logs, function(log){
+
+	if (log.level === 'info') {
+		logger.info(log.message);
+	}
+
+	else if (log.level === 'warning') {
+		logger.warning(log.message);
+	}
+
+	else if (log.level === 'error') {
+		logger.error(log.message);
+	}
+
+	else {
+		logger.log(log.level, log.message);
+	}
+
+});
