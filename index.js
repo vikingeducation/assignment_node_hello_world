2e34b1a2bee47d7163c12ce9e9653393ae7f7798
Ryan D. Barnett <conner2468@gmail.com>
2017-07-12 21:10:15 -0400

    Add JSON log

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var logger = require('./lib/logger');
 var _ = require('lodash');
-
-_.forEach([1, 2, 3], function(value) {
-  console.log(value);
-});
+var messages = require('./data/logs');
 
 logger.log('Message logged at INFO level', 'iNfO');
 logger.log('Message logged at WARNING level', 'WaRnIng');
 logger.log('Message logged at ERROR level', 'erRoR');
+
+_.forEach(messages, function(message) {
+  logger.log(message.message, message.level);
+});
