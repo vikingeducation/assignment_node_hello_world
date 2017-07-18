aa5f49aa76832a2cc97d58be8359118fa5578e99
Eleni A <eleni.arvanitis@me.com>
2017-07-18 17:18:34 -0400

    Added lib/logger.js and all tests to ensure everything is working

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var _ = require('lodash');
 const chalk = require('chalk');
+var logger = require('./lib/logger');
 
-const log = console.log;
+// Lines 5 through 12 were tests for chalk and lodash
+// console.log(chalk.blue('Hello ') + 'World' + chalk.red('!'));
 
-log(chalk.blue('Hello ') + 'World' + chalk.red('!'));
+// var sampleArr = ["this", "is", "a", 6, "index", "array"];
 
-var sampleArr = ["this", "is", "a", 6, "index", "array"];
+/* _.each(sampleArr, function(sampleArr) {
+  console.log(sampleArr);
+}); */
 
-_.each(sampleArr, function(sampleArr) {
-  log(sampleArr);
-});
+logger.log("Message logged at INFO level.", 'info');
+logger.log("Message logged at WARNING level.", 'WARNING');
+logger.log("Message logged at ERROR level.", 'eRROR');
