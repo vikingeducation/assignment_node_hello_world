d551da0b090794e191a0f1fafd33400588c0a806
Eleni A <eleni.arvanitis@me.com>
2017-07-19 16:12:35 -0400

    Added json file for data and changed parameter names

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 var _ = require('lodash');
-const chalk = require('chalk');
+// const chalk = require('chalk');
+var logs = require('./data/logs');
 var logger = require('./lib/logger');
 
-// Lines 5 through 12 were tests for chalk and lodash
-// console.log(chalk.blue('Hello ') + 'World' + chalk.red('!'));
+/* Lines 2, 7 through 13 were tests for chalk and lodash
+console.log(chalk.blue('Hello ') + 'World' + chalk.red('!'));
 
-// var sampleArr = ["this", "is", "a", 6, "index", "array"];
+var sampleArr = ["this", "is", "a", 6, "index", "array"];
 
-/* _.each(sampleArr, function(sampleArr) {
+ _.each(sampleArr, function(sampleArr) {
   console.log(sampleArr);
-}); */
+});
+
+Lines 16 through 20 were another test
 
 logger.log("Message logged at INFO level.", 'info');
 logger.log("Message logged at WARNING level.", 'WARNING');
 logger.log("Message logged at ERROR level.", 'eRROR');
+*/
+
+_.each(logs, function(logs) {
+  logger.log(logs.message, logs.level);
+});
