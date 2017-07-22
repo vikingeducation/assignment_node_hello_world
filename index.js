ad1f057e64fcb7e0d578f0bf3f6777eb78f5c534
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-07-21 17:11:47 -0700

    add my logger module

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var _ = require('lodash');
 var chalk = require('chalk');
+var logger = require('./lib/logger');
 
 console.log(chalk.blue('Hello world!'));
 
 _.each([1, 2], function(val) {
   console.log(val);
 });
+
+logger.log('hi', 'INFO');
+logger.log('hi', 'WARNING');
+logger.log('hi', 'ERROR');
+logger.info('hi');
+logger.warning('hi');
+logger.error('hi');
