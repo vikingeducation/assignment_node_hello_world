7e5057002bf859ea140a6d80ee10d137623ec287
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-07-21 17:26:25 -0700

    all done hooray

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var _ = require('lodash');
 var chalk = require('chalk');
 var logger = require('./lib/logger');
+var logs = require('./data/logs');
 
-console.log(chalk.blue('Hello world!'));
-
-_.each([1, 2], function(val) {
-  console.log(val);
+_.each(logs, function(currentLog) {
+  logger.log(currentLog.message, currentLog.level)
 });
-
-logger.log('hi', 'INFO');
-logger.log('hi', 'WARNING');
-logger.log('hi', 'ERROR');
-logger.info('hi');
-logger.warning('hi');
-logger.error('hi');
