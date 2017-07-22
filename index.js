c03595573e252d4ba78b89b4635159625add77ce
John Whittington <johncw90@yahoo.com>
2017-07-21 21:02:29 -0500

    Implemented logger

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-
-
 var chalk = require('chalk');
-
 var _ = require('lodash');
-
+var logger = require('./lib/logger');
 
 console.log(chalk.red('Hello ')+ chalk.blue('World!'));
 
@@ -12,3 +9,8 @@ var arr = [1,2,3,4,5];
 _.each(arr, function(arr) {
 	console.log(arr);
 });
+
+
+logger.info('Info level log');
+logger.warning('Warning level log');
+logger.error('Error level log');
