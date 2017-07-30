fb1941a4be17e91c6bcc13daaee043af7034dd3f
Chad Lucas <chad.lucas.19@gmail.com>
2017-07-29 23:10:50 -0500

    add logger function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!")
+var chalk = require('chalk');
+var _ = require('lodash');
+var data = require('./data/logs');
+var logger = require('./lib/logger');
+
+
+
+_.each(data, function(item){
+	logger.log(item.level + ' : ' + item.message);
+});
