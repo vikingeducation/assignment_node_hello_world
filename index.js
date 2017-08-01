d4ec4d5fffa59ccd8906f1cf723c319cd5dea4bd
anne <richardson.ae@gmail.com>
2017-08-01 10:56:32 -0500

    Comment out chalk and lodash examples

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger');
 
 
-const error = chalk.bold.red;
-const warning = chalk.keyword('orange');
+// ----------SAMPLE CHALK USAGE------------
+// const error = chalk.bold.red;
+// const warning = chalk.keyword('orange');
+// console.log(error('Error!'));
+// console.log(warning('Warning!'));
 
-console.log(error('Error!'));
-console.log(warning('Warning!'));
+// ----------SAMPLE LODASH USAGE------------
+// var kitties = {
+//   "alex": chalk.yellow("orange") + " swirls",
+//   "zorro": chalk.dim("black"),
+//   "violet": chalk.bgMagenta("wristkers")
+// };
+// _.each(kitties, function(cat){
+//   console.log(cat);
+// });
 
-
-var kitties = {
-  "alex": chalk.yellow("orange") + " swirls",
-  "zorro": chalk.dim("black"),
-  "violet": chalk.bgMagenta("wristkers")
-};
-
-_.each(kitties, function(cat){
-  console.log(cat);
-});
-
-var condiments = ["mustard", "ketchup", "sir kensington's"];
-
-_.each(condiments, function(condiment){
-  console.log(condiment);
-});
+// var condiments = ["mustard", "ketchup", "sir kensington's"];
+// _.each(condiments, function(condiment){
+//   console.log(condiment);
+// });
