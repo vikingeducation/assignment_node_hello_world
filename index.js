25112ce75dbff22d8690be359c3a8578519102ec
Jennifer Louie <louie.jen@gmail.com>
2017-08-07 20:28:10 -0700

    Added logging for different levels

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var _ = require('lodash');
 var chalk = require('chalk');
+var logger = require('./lib/logger');
 
-console.log(chalk.red("hello world!"));
+logger.log("Message logged at INFO level", 'INFO');
+logger.log("Message logged at WARNING level", 'WARNING');
+logger.log("Message logged at ERROR level", 'ERROR');
 
 _.each(["hello", "hola", "merhaba", "ni hao"], function(greeting) {
   console.log(greeting);
