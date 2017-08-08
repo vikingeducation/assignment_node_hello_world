ecf422d1d096b6804b021e491f7bcc3bdea6983e
Chuck Michael <chuckqs@yahoo.com>
2017-08-08 11:41:27 -0400

    Logger built

---
+++ index.js
@@ -0,0 +1,7 @@
+var chalked = require('chalk');
+var _ = require('lodash');
+var logger = require('./lib/logger');
+
+logger.log("Information", "INFO");
+logger.log("Warning", "WARNING");
+logger.log("Error", "ERROR");
