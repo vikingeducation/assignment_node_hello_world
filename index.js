5f233de552ca052bf1f7ac5ebc5e1513c4b9aab9
Chuck Michael <chuckqs@yahoo.com>
2017-08-08 11:45:47 -0400

    Iterated over json file with logger

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var chalked = require('chalk');
 var _ = require('lodash');
 var logger = require('./lib/logger');
+var logs = require("./data/logs");
 
-logger.log("Information", "INFO");
-logger.log("Warning", "WARNING");
-logger.log("Error", "ERROR");
+logger.log("Information", "info");
+logger.log("Warning", "warning");
+logger.log("Error", "error");
+
+_.each(logs, function(obj){
+  logger.log(obj.message, obj.level);
+})
