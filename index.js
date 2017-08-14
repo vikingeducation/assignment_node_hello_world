2c1fef365d55cb464486efa09498a967a4f185d1
Coelacanth7 <galford151@gmail.com>
2017-08-13 17:30:05 -0700

    finished json

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ console.log('hello world')
 
 var _ = require('lodash');
 var chalk = require('chalk');
+var logger = require('./lib/logger');
+var logs = require('./data/logs');
 
 console.log(chalk.blue('Hello World!'))
 
-
+//use lodash to iterate through array
 var simpleArray = ['red', 'green', 'blue']
 
 _.each(simpleArray, function(str){
   console.log(str);
 });
+
+//testing logging levels
+logger.log('logging some info', logger.info);
+logger.log('logging an error', logger.error);
+logger.log('logging a warning', logger.warning);
+
+//logging from JSON
+
+_.each(logs, function(entry){
+  logger.log(entry.message, logger[entry.level]);
+})
