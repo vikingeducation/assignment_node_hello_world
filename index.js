8b7c9a6eb025b95ce07d2f5b3e5409f2e8619b79
= <=>
2017-08-15 00:38:21 +0100

    Logger works fine

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const chalk = require('chalk');
 const _ = require('lodash');
+const logger = require('./lib/logger');
 
-let words = {
-	one: 'Hello world!',
-	two: "Hi There!",
-	three:"What\'s up?"
-};
-
-_.each(words, function (word) {
-	// body... 
-	console.log(chalk.red(word));
-});
+logger.log('hi');
