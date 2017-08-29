808b8fbde0050c0b581f0cca6cd1f3d660279f7c
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-08-29 11:26:55 +0200

    Working logger

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var _ = require('lodash');
-var chalk = require('chalk');
+var logger = require('./lib/logger');
 
-var myArray = ["yes", "no", "maybe"];
-_.each(myArray, function(element) {
-  console.log(chalk.green(element));
-});
+// var messages = ["Message logged at INFO level",
+//                 "Message logged at WARNING level",
+//                 "Message logged at ERROR level"];
+
+// _.each(messages, function(element) {
+//   logger.error(element);
+// });
+
+logger.info("Message logged at INFO level");
+logger.warning("Message logged at WARNING level");
+logger.error("Message logged at ERROR level");
+
+logger.log("Message logged at INFO level", "INFO");
+logger.log("Message logged at WARNING level", "WARNING");
+logger.log("Message logged at ERROR level", "ERROR");
