be28503aa9a09da080d9f3f5091364909a0a683d
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-08-29 11:32:57 +0200

    logger working with json

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var _ = require('lodash');
 var logger = require('./lib/logger');
+var logs = require('./data/logs');
 
-// var messages = ["Message logged at INFO level",
-//                 "Message logged at WARNING level",
-//                 "Message logged at ERROR level"];
-
-// _.each(messages, function(element) {
-//   logger.error(element);
-// });
-
-logger.info("Message logged at INFO level");
-logger.warning("Message logged at WARNING level");
-logger.error("Message logged at ERROR level");
-
-logger.log("Message logged at INFO level", "INFO");
-logger.log("Message logged at WARNING level", "WARNING");
-logger.log("Message logged at ERROR level", "ERROR");
+_.each(logs, function(element) {
+  logger.log(element.message, element.level);
+});
