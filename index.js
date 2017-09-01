7270015fdaf70278b51afe799f7a3a9183b6591c
Aaron Saloff <aaronsaloff@gmail.com>
2017-09-01 07:52:36 -0400

    logged data from JSON file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var _ = require('./node_modules/lodash');
 var logger = require('./lib/logger');
+var logs = require('./data/logs');
 
-logger.log("Message logged at INFO level", "INFO")
-logger.log("Message logged at WARNING level", "WARNING")
-logger.log("Message logged at ERROR level", "ERROR")
+_.each(logs, function(log) {
+  logger.log(log.message, log.level.toUpperCase());
+});
