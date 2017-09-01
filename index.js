119929197cbd872a93e71b1eed4900698dcace27
Aaron Saloff <aaronsaloff@gmail.com>
2017-08-31 22:40:21 -0400

    created color logger

---
+++ index.js
@@ -0,0 +1,6 @@
+var _ = require('./node_modules/lodash');
+var logger = require('./lib/logger');
+
+logger.log("Message logged at INFO level", "INFO")
+logger.log("Message logged at WARNING level", "WARNING")
+logger.log("Message logged at ERROR level", "ERROR")
