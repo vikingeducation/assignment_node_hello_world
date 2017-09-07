9f6631026f2e250ca36f8094f9671641a08b683e
Jared Gebel <jaredjgebel@gmail.com>
2017-09-07 12:36:07 -0400

    Complete JSON logger

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var chalk = require("chalk");
 var _ = require("lodash");
 var logger = require("./lib/logger");
+var logs = require("./data/logs");
 
 logger.log("This is a string", "INFO");
 logger.log("This is a warning", "WARNING");
@@ -9,3 +10,7 @@ logger.log("This is an error", "ERROR");
 logger.error("This is an error");
 logger.info("This is some info");
 logger.warning("this is a warning");
+
+_.forEach(logs, function(key) {
+  logger.log(key.message, key.level.toUpperCase());
+});
