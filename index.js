3d7770ec7060e27443d0482f36b62edebfd75acd
Brian Carroll <briancarroll112@gmail.com>
2017-09-08 08:46:19 -0400

    logger working at all levels

---
+++ index.js
@@ -0,0 +1,7 @@
+var _ = require('lodash');
+const chalk = require('chalk');
+var logger = require('./lib/logger');
+
+logger.info('Logged at info level');
+logger.warning('logged at warning level');
+logger.error('logged at error level');
