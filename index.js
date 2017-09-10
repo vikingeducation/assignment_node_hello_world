94cd40d717bde6deaa2586fa77e13e60b63ea38e
Paul Brown <pbrown581@gmail.com>
2017-09-10 14:15:28 -0400

    assignment node hello world

---
+++ index.js
@@ -0,0 +1,27 @@
+const chalk = require('chalk');
+// Load the full build. 
+var _ = require('lodash');
+// Load the core build. 
+var _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods. 
+var fp = require('lodash/fp');
+ 
+// Load method categories. 
+var array = require('lodash/array');
+var object = require('lodash/fp/object');
+ 
+// Cherry-pick methods for smaller browserify/rollup/webpack bundles. 
+var at = require('lodash/at');
+var curryN = require('lodash/fp/curryN');
+
+console.log(chalk.blue('hello world!'));
+
+var teams = require('./lib/favoriteTeams');
+var logger = require('./lib/logger');
+
+console.log(teams);
+console.log();
+
+_.each(teams, function(teams) {
+  logger.log(teams);
+});
