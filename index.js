7cb68db3dd0cfabc4935d88756c5266ff1aa25a9
Visiona <darbis@gmail.com>
2017-10-04 21:41:06 +0100

    created simple logging method colorizing output

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var _ = require('lodash');
 const chalk = require('chalk');
 
-console.log(chalk.blue('Hello World!'));
+var log = require('./lib/logger');
+log('This is error message', 'ERROR');
+log('This is warning message', 'WARNING');
+log('This is info message', 'INFO');
 
-var arr = [1,2,3,4];
-
-_.each(arr, function(element) {
-  console.log(element);
-});
+// console.log(chalk.blue('Hello World!'));
+//
+// var arr = [1,2,3,4];
+//
+// _.each(arr, function(element) {
+//   console.log(element);
+// });
