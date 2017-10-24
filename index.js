c79afc4d8b4c2cfa0754854b1b5f701e12262647
Hai Nguyen <memyselfandhai@gmail.com>
2017-10-23 20:19:24 -0500

    logged completed

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var _ = require('lodash');
 const chalk = require('chalk');
+var logger = require('./lib/logger')
 
-
+//chalk works
 console.log(chalk.green(
     'I am a green line ' +
     chalk.blue.underline.bold('with a blue substring') +
     ' that becomes green again!'
 ));
 
+//lodash works
 const array = [1,2,3];
 
 _.each (array, function(n) {
   console.log(`${n} potato`);
 })
+
+
+logger.log('Messaged logged at ERROR level','ERROR');
