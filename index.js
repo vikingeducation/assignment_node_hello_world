81f7abbde8d72692f4c706f09224d72383c5ff99
Elias Hantula <ehantula@indiana.edu>
2017-10-31 12:54:49 -0400

    logger color

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const _ = require('lodash');
 const chalk = require('chalk')
-const log = console.log;
-var arr = [1,2,3]
-
+var logger = require('./lib/logger')
+var arr = [1,2,3];
 
 _.each(arr, function(number){
 
-log(chalk.blue(number));
+console.log(chalk.blue(number));
 })
-log(chalk.blue("Hello world!"))
+console.log(chalk.blue("Hello world!"));
+
+
+logger.log("testing", "INFO")
+logger.log("testing", "WARNING")
+logger.log("testing", "ERROR")
