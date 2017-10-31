58e52539389b155a68421434c3ff1af2784f2379
Jeff Dederick <Xpressivmotion@gmail.com>
2017-10-31 11:23:12 -0400

    project functional and complete

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 let _ = require('lodash');
 let chalk = require('chalk');
 let logger = require('./lib/logger.js');
+let messages = require('./data/logs.json');
 
-logger.log('INFO level message', 'info');
-logger.log('WARNING level message', 'warning');
-logger.log('ERROR level message', 'error');
+// logger.log('INFO level message', 'info');
+// logger.log('WARNING level message', 'warning');
+// logger.log('ERROR level message', 'error');
+
+_.each(messages, function(x) {
+  return logger.log(x.level, x.message);
+});
