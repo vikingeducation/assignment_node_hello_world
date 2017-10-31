031faebab649ea29493281dc50fb4b4fc0246018
Elias Hantula <ehantula@indiana.edu>
2017-10-31 13:57:35 -0400

    logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const chalk = require('chalk')
+const logs = require('./data/logs')
 var logger = require('./lib/logger')
 var arr = [1,2,3];
 
@@ -9,7 +10,13 @@ console.log(chalk.blue(number));
 })
 console.log(chalk.blue("Hello world!"));
 
+_.each(logs, function(msg){
+  logger.log(msg.message, msg.level );
 
-logger.log("testing", "INFO")
-logger.log("testing", "WARNING")
-logger.log("testing", "ERROR")
+
+
+})
+
+logger.log("testing", "info")
+logger.log("testing", "warning")
+logger.log("testing", "error")
