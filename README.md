5aa929d5180fa29e725fa507e4ae57d326c392ff
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-04 18:32:55 -0700

    Able to log message at all logging levels

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 assignment_node_hello_world
 ===========================
 
-
 Just Node.js saying hello to the world and such.
 
-
-
-
+Hi.. this is Lakshmi Maduri
--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const _chalk = require('chalk');
 const _lodash = require('lodash');
 
-var salutations = require('./lib/salutations');
+var logger = require('./lib/logger');
 
-var arr = [1,2,3,4];
+logger.info = function(msg){
+  console.log(_chalk.blue(msg));
+};
 
-_lodash.each(arr, function(elem){
-  console.log(elem);
-})
+logger.error = function(msg){
+  console.log(_chalk.red(msg));
+};
 
-_lodash.each(salutations, function(salutation){
-  console.log(salutation);
-});
+logger.warning = function(msg){
+  console.log(_chalk.yellow(msg));
+};
 
-console.log(_chalk.blue('Hello') + " " + _chalk.red('world!'));
+logger.log("Logging message at the INFO level", "INFO");
+logger.log("Logging message at the ERROR level", "ERROR");
+logger.log("Logging message at the WARNING level", "WARNING");
 
-console.log("Node.js says Hello world!");
+//Testing lodash and chalk
+// var salutations = require('./lib/salutations');
+//
+// var arr = [1,2,3,4];
+//
+// _lodash.each(arr, function(elem){
+//   console.log(elem);
+// })
+//
+// _lodash.each(salutations, function(salutation){
+//   console.log(salutation);
+// });
+//
+// console.log(_chalk.blue('Hello') + " " + _chalk.red('world!'));
+//
+// console.log("Node.js says Hello world!");
