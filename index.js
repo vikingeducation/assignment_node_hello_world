264bfc194167baa8902a1818bb32e5bb012d0928
Johann <baptistajohann@gmail.com>
2017-11-10 08:31:39 -0800

    logger working

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let _ = require('lodash');
 let chalk = require('chalk');
+let Logger = require('./lib/logger');
+
+let logger = new Logger();
 
 console.log(chalk.blue('Hello') + 'World' + chalk.red('!'));
 
@@ -9,4 +12,7 @@ _.each([1,2,3], function(el, idx) {
 
 console.log("hello world");
 
+console.log(logger.log("This should be blue", "INFO"));
+console.log(logger.log("This should be yellow", "WARNING"));
+console.log(logger.log("This should be red", "ERROR"));
 
