a3d119ab5e440226da19b69c4c9937efb86a6087
Austin1780 <austinwilliamsmith@gmail.com>
2017-11-12 15:31:23 -0700

    final additions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 let _ = require("lodash");
 let chalk = require("chalk");
+let logger = require("./lib/logger");
+let list = require("./data/logs");
 
 console.log(chalk.blue("hello world"));
 
@@ -8,4 +10,12 @@ let simpleArray = [1, 2, 3];
 
 _.each(simpleArray, (num) => {
   console.log(num);
+});
+
+logger.log("blue", "INFO");
+logger.log("yellow", "WARNING");
+logger.log("red", "ERROR");
+
+_.each(list, function(obj, idx) {
+  logger.log(obj.message, obj.level);
 });
