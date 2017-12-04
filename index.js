4379118882ab3b713d1f9e21f3b821e410bec788
Holly Erickson <Erickson.Holly@icloud.com>
2017-12-04 14:04:59 -0600

    logger built

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const chalk = require('chalk');
 var _ = require('lodash');
 
-let arr=["hi", 2, false];
+// let arr=["hi", 2, false];
+//
+// function log(el) {
+//   console.log(el);
+// }
+// _.each(arr, log);
 
-function log(el) {
-  console.log(el);
-}
 
-console.log(chalk.bgBlue("Blue!"));
-_.each(arr, log);
+var logger = require('./lib/logger');
+
+logger.log('Message logged at the INFO level', 'info');
+logger.log('Message logged at the WARNING level', 'warning');
+logger.log('Message logged at the ERROR level', 'error');
