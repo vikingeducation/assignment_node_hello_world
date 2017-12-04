ff7c124c1adba6bc2c3476a491368078cbfdb04c
Holly Erickson <Erickson.Holly@icloud.com>
2017-12-04 14:18:23 -0600

    able to output from JSON file using logger

--- index.js
+++ index.js
@@ -2,16 +2,13 @@
 const chalk = require('chalk');
 var _ = require('lodash');
 
-// let arr=["hi", 2, false];
-//
-// function log(el) {
-//   console.log(el);
-// }
-// _.each(arr, log);
-
-
 var logger = require('./lib/logger');
+var data = require('./data/logs');
 
 logger.log('Message logged at the INFO level', 'info');
 logger.log('Message logged at the WARNING level', 'warning');
 logger.log('Message logged at the ERROR level', 'error');
+
+_.each(data, function(data) {
+  logger.log(data.message, data.level);
+});
