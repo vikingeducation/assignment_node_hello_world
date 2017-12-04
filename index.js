1ea634eb1a835d6cd57df878a4dba29a52f600dd
Holly Erickson <Erickson.Holly@icloud.com>
2017-12-04 14:25:00 -0600

    Revert to uppercase levels

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var _ = require('lodash');
 var logger = require('./lib/logger');
 var data = require('./data/logs');
 
-logger.log('Message logged at the INFO level', 'info');
-logger.log('Message logged at the WARNING level', 'warning');
-logger.log('Message logged at the ERROR level', 'error');
+logger.log('Message logged at the INFO level', 'INFO');
+logger.log('Message logged at the WARNING level', 'WARNING');
+logger.log('Message logged at the ERROR level', 'ERROR');
 
 _.each(data, function(data) {
-  logger.log(data.message, data.level);
+  logger.log(data.message, data.level.toUpperCase());
 });
