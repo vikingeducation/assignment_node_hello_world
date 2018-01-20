944d14a54a47b0e4deeb55ad08aa7349c1727f71
John Sawyer <sawyer@sawyermerchant.com>
2018-01-20 05:48:27 -0500

    Add logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger');
 
 console.log(chalk.blue('hey there'));
 
@@ -8,3 +9,13 @@ var colors = ['red', 'blue', 'green'];
 _.each(colors, function(color){
   console.log(color);
 })
+
+// var levels = ['info', 'warning', 'error']
+//
+// _.each(levels, function(level){
+//   logger.level("This is a message of type " + level);
+// })
+
+logger.info("this is an info message");
+logger.warning("this is a warning message");
+logger.error("this is an error message");
