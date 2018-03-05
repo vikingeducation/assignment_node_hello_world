88ebf91ada599201d21364c7ce7cd0bfc71ad0b0
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-03-04 20:47:47 -0500

    finished turning logger into functional module

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 
-console.log("Hello World!");
+/*
+  cd Documents/Viking/Node/hello_world
+
+  https://www.vikingcodeschool.com/dashboard#/professional-development-with-javascript/node-js-hello-world
+*/
 
 const chalk = require('chalk');
-console.log(chalk.red('I am chalk'));
-
 var _ = require('lodash');
+var logger = require("./lib/logger");
+
+console.log("Hello World!");
 console.log(_.name);
+console.log(chalk.red('chalk'));
 
 _.each([1, 2, 3], function(value) {
   console.log(value);
 });
+
+logger.log("test", "info");
+logger.log("123", "warn");
+logger.log("abc", "error");
