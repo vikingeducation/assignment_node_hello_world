fd2ef9061197c578c918072837f173986ba8ec96
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-03-04 23:04:43 -0500

    ran js files through prettier

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 
-/*
-  cd Documents/Viking/Node/hello_world
-
-  https://www.vikingcodeschool.com/dashboard#/professional-development-with-javascript/node-js-hello-world
-*/
-
-const chalk = require('chalk');
-var _ = require('lodash');
+const chalk = require("chalk");
+var _ = require("lodash");
 var logger = require("./lib/logger");
-var jason = require("./data/logs");
+var record = require("./data/logs");
 
 console.log("Hello World!");
+console.log(chalk.red("chalk"));
 console.log(_.name);
-console.log(chalk.red('chalk'));
 
 _.each([1, 2, 3], function(value) {
   console.log(value);
@@ -22,6 +16,6 @@ logger.log("test", "info");
 logger.log("123", "warning");
 logger.log("abc", "error");
 
-_.each(jason, function(value, index) {
+_.each(record, function(value, index) {
   logger.log(value.message, value.level);
 });
