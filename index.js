f75fd7c16718eb456484a49312afe2dc196738b8
Kenny Kottenstette <ken@Kennys-MacBook-Pro.local>
2018-08-25 01:23:37 -0700

    begin testing

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ var names = ["bob", "fred", "will"];
 _.each(names, function(name){
   console.log(chalk.blue(name))
 })
+
+console.log()
+console.log()
+console.log()
+console.log()
+
+//warmup end
+
+var logger = require("./lib/logger");
+logger.log('info level', 'info')
+logger.log('warning level', 'warning')
+logger.log('error level', 'error')
+console.log();
+logger.info("info");
+logger.warning("warning");
+logger.error("error");
